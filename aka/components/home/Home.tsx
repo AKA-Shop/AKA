c468a2213204015b10185086bf6980f2988bbfbd
amineamdouni <amineamdouni24@gmail.com>
2022-12-13 16:33:08 +0100

    home page

--- aka/components/home/Home.tsx
+++ aka/components/home/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import Carousel  from 'react-bootstrap/carousel'
-import Image from 'next/image'
+// import Carousel from 'react-bootstrap/Carousel';
+// import Image from 'next/image'
+import Car from '../carousel/Carousel';
+import NavBar from '../navbar/NavBar';
 const Home = () => {
   return (
       <>
@@ -28,165 +30,11 @@ const Home = () => {
   <div id="preloder">
     <div className="loader" />
   </div>
-  {/* Offcanvas Menu Begin */}
-  <div className="offcanvas-menu-overlay" />
-  <div className="offcanvas-menu-wrapper">
-    <div className="offcanvas__option">
-      <div className="offcanvas__links">
-        <a href="#">Sign in</a>
-        <a href="#">FAQs</a>
-      </div>
-      <div className="offcanvas__top__hover">
-        <span>
-          Usd <i className="arrow_carrot-down" />
-        </span>
-        <ul>
-          <li>USD</li>
-          <li>EUR</li>
-          <li>USD</li>
-        </ul>
-      </div>
-    </div>
-    <div className="offcanvas__nav__option">
-      <a href="#" className="search-switch">
-        <img src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670922313/AKA/search_kbkgaw.png" alt="" />
-      </a>
-      <a href="#">
-        <img src="/aka/img/icon/heart.png" alt="" />
-      </a>
-      <a href="#">
-        <img src="/aka/img/icon/cart.png" alt="" /> <span>0</span>
-      </a>
-      <div className="price">$0.00</div>
-    </div>
-    <div id="mobile-menu-wrap" />
-    <div className="offcanvas__text">
-      <p>Free shipping, 30-day return or refund guarantee.</p>
-    </div>
-  </div>
-  {/* Offcanvas Menu End */}
-  {/* Header Section Begin */}
-  <header className="header">
-    <div className="header__top">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-6 col-md-7">
-            <div className="header__top__left">
-              <p>Free shipping, 30-day return or refund guarantee.</p>
-            </div>
-          </div>
-          <div className="col-lg-6 col-md-5">
-            <div className="header__top__right">
-              <div className="header__top__links">
-                <a href="#">Sign in</a>
-                <a href="#">FAQs</a>
-              </div>
-              <div className="header__top__hover">
-                <span>
-                  Usd <i className="arrow_carrot-down" />
-                </span>
-                <ul>
-                  <li>USD</li>
-                  <li>EUR</li>
-                  <li>USD</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-3 col-md-3">
-          <div className="header__logo">
-            <a href="./index.html">
-              <img src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670925252/aka/logo/logo_zkivkb.png" alt="" />
-            </a>
-          </div>
-        </div>
-        <div className="col-lg-6 col-md-6">
-          <nav className="header__menu mobile-menu">
-            <ul>
-              <li className="active">
-                <a href="./index.html">Home</a>
-              </li>
-              <li>
-                <a href="./shop.html">Shop</a>
-              </li>
-              <li>
-                <a href="#">Pages</a>
-                <ul className="dropdown">
-                  <li>
-                    <a href="./about.html">About Us</a>
-                  </li>
-                  <li>
-                    <a href="./shop-details.html">Shop Details</a>
-                  </li>
-                  <li>
-                    <a href="./shopping-cart.html">Shopping Cart</a>
-                  </li>
-                  <li>
-                    <a href="./checkout.html">Check Out</a>
-                  </li>
-                  <li>
-                    <a href="./blog-details.html">Blog Details</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="./blog.html">Blog</a>
-              </li>
-              <li>
-                <a href="./contact.html">Contacts</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div className="col-lg-3 col-md-3">
-          <div className="header__nav__option">
-            <a href="#" className="search-switch">
-              <img src="/aka/img/icon/search.png" alt="" />
-            </a>
-            <a href="#">
-              <img src="/aka/img/icon/heart.png" alt="" />
-            </a>
-            <a href="#">
-              <img src="/aka/img/icon/cart.png" alt="" /> <span>0</span>
-            </a>
-            <div className="price">$0.00</div>
-          </div>
-        </div>
-      </div>
-      <div className="canvas__open">
-        <i className="fa fa-bars" />
-      </div>
-    </div>
-  </header>
+ <NavBar></NavBar>
   {/* Header Section End */}
   {/* Hero Section Begin */}
  <section className="hero">
-     <div className="main-carousel">
-      <div className="our-product">
-        <Carousel fade>
-          <Carousel.Item>
-            <img
-              className="d-block w-100  image-product"
-              src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670925680/aka/hero/hero-1_olnrha.jpg"
-              alt="First slide"
-            />
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100 image-product"
-              src="https://res.cloudinary.com/dugewmeeh/image/upload/v1670925680/aka/hero/hero-2_qnu7hz.jpg"
-              alt="Second slide"
-            />
-          </Carousel.Item>
-          
-        </Carousel>
-      </div>
-    </div>
+    <Car></Car>
 </section>
 
   {/* Hero Section End */}
