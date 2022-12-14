d5ede9337c0d53a6e13ae329d55fb3ba4fb9f55a
karimabdelehi <karimabdelhedi080@gmail.com>
2022-12-14 14:50:25 +0100

    test

--- aka/components/home/Home.tsx
+++ aka/components/home/Home.tsx
@@ -331,7 +331,7 @@ const Home = () => {
             <p>
               Copyright © 2020 All rights reserved | This template is made with{" "}
               <i className="fa fa-heart-o" aria-hidden="true" /> by{" "}
-              <a href="https://colorlib.com" target="_blank">
+              <a href="https://colorlib.com" target="_blank" rel="noreferrer">
                 Colorlib
               </a>
             </p>
