895fc55347e1e9fb6e8b2cab3b7f1e9ac43e2e40
Ahlem5br <benromdhaneahlem84@gmail.com>
2022-12-14 14:43:04 +0100

    fixed the authentication backend

--- server/models/cart.model.js
+++ server/models/cart.model.js
@@ -4,7 +4,9 @@ const Cart = mongoose.model(
   "Cart",
   new mongoose.Schema({
     nameArticle:  {type:String},
+
     price: {type:Number},
+    
     quantity:{type:Number},
     image:{type:String}
   })
